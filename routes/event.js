69bdb5bbf3dcb19edb9c196e3f58624c735ed2d8
shenyuancheng <alfredshen1989@sina.com>
2018-03-26 17:59:42 +0800

    's'

--- routes/event.js
+++ routes/event.js
@@ -50,7 +50,7 @@ router.get('/', function(req, res, next) {
         }
     })
 
-    res.render('about/mainview/event', {
+    res.render('mainview/event', {
         title: '集团简介_关于商赢-商赢集团官网',
         description: '商赢集团通过资本助力，科技创新，以打造“消费新生态，金融新生态”为企业使命，立志成为公众信赖，值得托付的全球化多平台产融集团，实现“商者无域，相融共赢”的企业愿景！',
         keywords: '集团简介,商赢,商赢集团',
