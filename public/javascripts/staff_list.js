ed21977e35381fa48cda6f2bcd80601f8d493455
shenyuancheng <alfredshen1989@sina.com>
2018-03-29 19:35:40 +0800

    'swiper'

--- public/javascripts/staff_list.js
+++ public/javascripts/staff_list.js
@@ -1,7 +1,56 @@
     var viewIndex = parseInt($("#viewIndex").val());
     var pageCount = parseInt($("#pageCount").val());
+    var id = $("#dataId").val();
+    var swiper_mask = $(".swiper-mask");
+    var staff_swiper = $("#staff-swiper");
+    var staff_slide = $(".staff-slide");
+
+    var swiper = {
+        object: null,
+        initialSlide: 0,
+        initial: function() {
+            this.object = new Swiper('#staff-swiper',{
+                initialSlide: this.initialSlide,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev'
+            });
+        }
+    };
+
+    //初始化加载
+    $.support.cors=true;
+    $.ajax({
+        type: 'get',
+        dataType:"json",
+        url: 'http://gsite.shangyingjt.com/api/style/'+id+"?pageSize=6&iPage="+1,
+        success: function (response) {
+            for(var j=0; j<response.data.picUrlList.length; j++) {
+                staff_swiper.find('.swiper-wrapper').append('<div class="swiper-slide"><div class="swiper-img-box"><img src="'+ response.data.picUrlList[j] +'" /></div></div>')
+            }
+
+            staff_slide.on('click', 'li', function() {
+                var index = $(this).index();
+                swiper_mask.fadeIn();
+                $("body").css({
+                    overflow: 'overflow'
+                });
+                swiper.initialSlide = parseInt(index);
+                swiper.initial();
+            });
+        }
+    });
+    //关闭swiper 并销毁
+    swiper_mask.on('click', '.swiper-box-close', function() {
+        swiper_mask.fadeOut(function() {
+            swiper.object.setWrapperTranslate(0);
+            swiper.object.destroy();
+        });
+       $("body").css({
+           overflow: 'auto'
+       });
+    });
+    //分页点击
     $('.pagingtion_btn').on('click',function(e) {
-        var id = $("#dataId").val();
         if ($(this).hasClass('prev')) {
             viewIndex--;
         } else if ($(this).hasClass('next')) {
@@ -15,7 +64,6 @@
         } else {
             return;
         }
-
         if(viewIndex > 1 ) {
             $(".home").removeClass("button_text").addClass('page_btn');
             $(".prev").removeClass("button_text").addClass('page_btn');
@@ -38,7 +86,7 @@
         $('.pagingtion_btn.num').addClass('page_btn')
             .removeClass('button_text').eq(viewIndex-1)
             .addClass('button_text')
-            .removeClass('.page_btn')
+            .removeClass('.page_btn');
 
         $.support.cors=true;
         $.ajax({
@@ -52,9 +100,12 @@
                     var li =$("<li><img src='"+response.data.picUrlList[i] +"' alt="+ response.data.name +"/></li>");
                     staff_slide.append(li);
                 }
-                // if(viewIndex == response.iPage) {
-                //     $('.pagingtion_btn.num').eq(viewIndex).addClass('button_text').removeClass('.page_btn');
-                // }
+
+                staff_swiper.find('.swiper-wrapper').empty();
+                for(var j=0; j<response.data.picUrlList.length; j++) {
+                    staff_swiper.find('.swiper-wrapper').append('<div class="swiper-slide"><div class="swiper-img-box"><img src="'+ response.data.picUrlList[j] +'" /></div></div>')
+                }
+                swiper.initial();
             }
         });
 
---
+++ public/javascripts/staff_mulit_list.js
@@ -0,0 +1,63 @@
+var swiper_mask = $(".swiper-mask");
+var staff_swiper = $("#staff-swiper");
+var staff_slide = $(".staff-slide");
+var swiper_box = $(".swiper-box");
+var index, parent_index;
+
+var swiper = [
+    {
+        object: null,
+        initialSlide: 0,
+        initial: function() {
+            this.object = new Swiper('#staff-swiper0', {
+                initialSlide: this.initialSlide,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev'
+            });
+        }
+    },
+    {
+        object: null,
+        initialSlide: 0,
+        initial: function() {
+            this.object = new Swiper('#staff-swiper1', {
+                initialSlide: this.initialSlide,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev'
+            });
+        }
+    },
+    {
+        object: null,
+        initialSlide: 0,
+        initial: function() {
+            this.object = new Swiper('#staff-swiper2', {
+                initialSlide: this.initialSlide,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev'
+            });
+        }
+    }
+]
+
+staff_slide.on('click', 'li', function() {
+    index = parseInt($(this).attr("data-id"));
+    parent_index = $(this).parent().attr("data-id");
+    $("body").css({
+        overflow: 'overflow'
+    });
+    swiper_mask.fadeIn();
+    swiper_box.eq(parent_index).fadeIn();
+    swiper[parent_index].initialSlide = index;
+    swiper[parent_index].initial()
+});
+
+//关闭swiper 并销毁
+swiper_mask.on('click', '.swiper-box-close', function() {
+    swiper_mask.fadeOut();
+    swiper_box.eq(parent_index).fadeOut();
+    $("body").css({
+        overflow: 'auto'
+    });
+    swiper[parent_index].object.destroy();
+});
